566aa83d3e8bb8800936e06c9833c0a35a3610de
Karan SIngh <karan7a43@gmail.com>
2026-07-19 22:27:37 +0530

    Time: 14 ms (31.08%), Space: 46.5 MB (20.42%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -2,13 +2,13 @@ class Solution {
     public boolean isPalindrome(String s) {
         s = s.toLowerCase().replaceAll("[^a-z0-9]","");
         int start = 0;
-        int end = s.length() -1 ;
+        int end = s.length() - 1;
         while(start <= end){
-            if(s.charAt(start) != s.charAt(end)){
-                return false;
+            if(s.charAt(start) == s.charAt(end)){
+                start++;
+                end--;
             }
-            start++;
-            end--;
+            else return false;
         }
         return true;
     }
