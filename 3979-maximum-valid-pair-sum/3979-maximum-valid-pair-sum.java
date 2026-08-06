4201439d0038e91a4f40c2e4550459ffb920f532
Karan SIngh <karan7a43@gmail.com>
2026-08-06 23:40:37 +0530

    Time: 1 ms (100%), Space: 118.6 MB (82.81%) - LeetHub

---
+++ 3979-maximum-valid-pair-sum/3979-maximum-valid-pair-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int maxValidPairSum(int[] nums, int k) {
+        
+        int max1 = Integer.MIN_VALUE; // maximum left value
+        int max2 = Integer.MIN_VALUE; // answer
+        
+        int i = k;
+
+        while(i < nums.length) {
+            
+
+            if(max1 < nums[i-k]) {
+                max1 = nums[i-k];
+            }
+
+            if(max2 < max1 + nums[i]) {
+                max2 = max1 + nums[i];
+            }
+
+            i++;
+        }
+
+        return max2;
+    }
+}
