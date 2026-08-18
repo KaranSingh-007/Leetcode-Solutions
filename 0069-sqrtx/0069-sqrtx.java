06ad134e0f4c0b0915d405338a0fd6b3962917a8
Karan SIngh <karan7a43@gmail.com>
2026-08-18 23:53:52 +0530

    Time: 1 ms (98.19%), Space: 42.6 MB (61.96%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -4,10 +4,14 @@ class Solution {
         long end = x;
         long mid = 0;
         while(start <= end){
-            mid = (start+end)/2;
-            if(mid*mid == x) return (int)mid;
-            else if (mid*mid > x) end = mid-1;
-            else start = mid + 1;
+            mid = (start +end)/2;
+            if(mid*mid == x){
+                return (int)mid;
+            }
+            else if(mid*mid > x){
+                end = mid-1;
+            }
+            else start = mid+1;
         }
         return (int)end;
     }
