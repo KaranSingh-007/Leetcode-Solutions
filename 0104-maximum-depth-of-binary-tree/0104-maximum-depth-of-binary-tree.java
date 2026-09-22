647b5136f16559f898564a3af60a5a2d88a52b0e
Karan Singh <karan7a43@gmail.com>
2026-09-22 23:16:21 +0530

    Time: 0 ms (100%), Space: 46.9 MB (66.74%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.java
@@ -1,25 +1,29 @@
 class Solution {
     public int maxDepth(TreeNode root) {
-        Queue<TreeNode> elementqueue = new LinkedList<>();
+        // Queue<TreeNode> elementqueue = new LinkedList<>();
+        // if(root == null) return 0;
+        // elementqueue.add(root);
+        // int numberoflevels = 0;
+        // while(true){
+        //     int nodecount = elementqueue.size();
+        //     if(nodecount == 0){
+        //         return numberoflevels;
+        //     }
+        //     while(nodecount > 0){
+        //         TreeNode element = elementqueue.poll();
+        //         if(element.left != null){
+        //             elementqueue.add(element.left);
+        //         }
+        //         if(element.right != null){
+        //             elementqueue.add(element.right);
+        //         }
+        //         nodecount--;
+        //     }
+        //     numberoflevels++;
+        // }
         if(root == null) return 0;
-        elementqueue.add(root);
-        int numberoflevels = 0;
-        while(true){
-            int nodecount = elementqueue.size();
-            if(nodecount == 0){
-                return numberoflevels;
-            }
-            while(nodecount > 0){
-                TreeNode element = elementqueue.poll();
-                if(element.left != null){
-                    elementqueue.add(element.left);
-                }
-                if(element.right != null){
-                    elementqueue.add(element.right);
-                }
-                nodecount--;
-            }
-            numberoflevels++;
-        }
+        int lh = maxDepth(root.left);
+        int rh = maxDepth(root.right);
+        return Math.max(lh , rh) + 1;
     }
 }
