3ba6a664aa4f5e3a6d8c3e4bc1973918fbd2b4db
Karan Singh <karan7a43@gmail.com>
2026-09-25 23:25:16 +0530

    Time: 0 ms (100%), Space: 42.9 MB (45.89%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.java
+++ 0226-invert-binary-tree/0226-invert-binary-tree.java
@@ -1,32 +1,32 @@
 class Solution {
     public TreeNode invertTree(TreeNode root) {
         // Recurssive Technique
-        // if(root == null){
-        //     return null;
-        // }
-        // TreeNode left = invertTree(root.left);
-        // TreeNode right = invertTree(root.right);
-        // root.left = right;
-        // root.right = left;
-        // return root;
-        //Stack Technique
         if(root == null){
             return null;
         }
-        Stack<TreeNode> stack = new Stack<>();
-        stack.push(root);
-        while(!stack.isEmpty()){
-            TreeNode node = stack.pop();
-            TreeNode temp = node.left;
-            node.left = node.right;
-            node.right = temp;
-            if(node.left != null){
-                stack.push(node.left);
-            }
-            if(node.right != null){
-                stack.push(node.right);
-            }
-        }
+        TreeNode left = invertTree(root.left);
+        TreeNode right = invertTree(root.right);
+        root.left = right;
+        root.right = left;
         return root;
+        //Stack Technique
+        // if(root == null){
+        //     return null;
+        // }
+        // Stack<TreeNode> stack = new Stack<>();
+        // stack.push(root);
+        // while(!stack.isEmpty()){
+        //     TreeNode node = stack.pop();
+        //     TreeNode temp = node.left;
+        //     node.left = node.right;
+        //     node.right = temp;
+        //     if(node.left != null){
+        //         stack.push(node.left);
+        //     }
+        //     if(node.right != null){
+        //         stack.push(node.right);
+        //     }
+        // }
+        // return root;
     }
 }
